18bfdfea03e31fa0c9d171a0d24a9380eaef5e0f
achitJ <ajain2801@gmail.com>
2023-05-30 20:30:30 +0530

    completed contact me form

--- src/pages/api/telegram-form.ts
+++ src/pages/api/telegram-form.ts
@@ -8,8 +8,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             //send data to telegram
             const data = req.body;
             data.dateTime = new Date(data.dateTime);
-            const response = await sendDataToTelegram(data);
-            return res.status(200).json({ message: 'OK', data, response });
+            await sendDataToTelegram(data);
+            return res.status(200).json({ message: 'OK', data });
         } catch(error) {
             return res.status(500).json({ message: 'Internal Server Error', error });
         }
